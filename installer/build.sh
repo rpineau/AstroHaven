4c91702842b22d0443849e6e28e52dc3a7674a54
Rodolphe Pineau <pineau@rti-zone.org>
2020-01-02 12:00:18 -0800

    Fix macOS 64 bit code signing

--- installer/build.sh
+++ installer/build.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+PACKAGE_NAME="AstroHaven_X2.pkg"
+BUNDLE_NAME="org.rti-zone.AstroHavenX2"
+
+if [ ! -z "$app_id_signature" ]; then
+    codesign -f -s "$app_id_signature" --verbose ../build/Release/libAstroHaven.dylib
+fi
+
 mkdir -p ROOT/tmp/AstroHaven_X2/
 cp "../domelist AstroHaven.txt" ROOT/tmp/AstroHaven_X2/
 cp "../build/Release/libAstroHaven.dylib" ROOT/tmp/AstroHaven_X2/
 
+
 if [ ! -z "$installer_signature" ]; then
-# signed package using env variable installer_signature
-pkgbuild --root ROOT --identifier org.rti-zone.AstroHaven_X2 --sign "$installer_signature" --scripts Scripts --version 1.0 AstroHaven_X2.pkg
-pkgutil --check-signature ./AstroHaven_X2.pkg
+	# signed package using env variable installer_signature
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --sign "$installer_signature" --scripts Scripts --version 1.0 $PACKAGE_NAME
+	pkgutil --check-signature ./${PACKAGE_NAME}
 else
-pkgbuild --root ROOT --identifier org.rti-zone.AstroHaven_X2 --scripts Scripts --version 1.0 AstroHaven_X2.pkg
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --scripts Scripts --version 1.0 $PACKAGE_NAME
 fi
 
 rm -rf ROOT
